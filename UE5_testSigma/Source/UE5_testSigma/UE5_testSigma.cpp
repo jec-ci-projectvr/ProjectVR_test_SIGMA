4326f1a75cacbd2aad9a7a2a9f02cd2975456849
IinoCI04 <117345388+TasukuIino@users.noreply.github.com>
2023-05-31 18:32:48 +0900

    Delete UE5_testSigma directory
    いらない

--- UE5_testSigma/Source/UE5_testSigma/UE5_testSigma.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE5_testSigma.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_testSigma, "UE5_testSigma" );
