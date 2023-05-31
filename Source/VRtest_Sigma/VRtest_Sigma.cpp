cf771308a7cc693ab79b4c4b9bf1229324d2bc13
22CI04_iino <22ci0403@jec.ac.jp>
2023-05-31 18:10:27 +0900

    Revert "初期データを再再再プッシュ"
    This reverts commit 87c71a0ec6647debc6cb9e248e752be25e7d8809.

--- Source/VRtest_Sigma/VRtest_Sigma.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "VRtest_Sigma.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRtest_Sigma, "VRtest_Sigma" );
