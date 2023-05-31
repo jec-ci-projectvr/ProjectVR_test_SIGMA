d4a9f9b9c3551322edc01cedef031d3638dbbcb3
22CI04_iino <22ci0403@jec.ac.jp>
2023-05-31 18:34:03 +0900

    初期データを再再再再再プッシュ

---
+++ Source/UE5_testSigma/UE5_testSigma.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_testSigma.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_testSigma, "UE5_testSigma" );
